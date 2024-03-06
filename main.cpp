8859698e9690347a0ddb540fc5f749d2dffd2c9c
Darsh <68748407+Blastemperor406@users.noreply.github.com>
2024-03-06 17:29:22 +0530

    Create main.cpp
    I spelt pipeline wrong

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "I am Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Pipepline n-n";
+  return 0;
+}
